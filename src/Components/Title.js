a6a7471a2d87cb81f47d44986f044dc313a5899b
latoulicious <count.kramm48@gmail.com>
2022-04-09 21:04:10 +0700

    🎨 Improve Code Structure

---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../CSS/Title.css';
+
+function Title() {
+    return (
+      <div className="Title">
+        <h1>latoulicious</h1>
+      </div>
+    )
+  }
+
+  export default Title
