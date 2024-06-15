dfa0a29550a6b3361c86efe7754b151654270827
arsyds <arsyds@proton.me>
2024-06-15 11:03:17 +0700

    docs: improve project README

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# Mantine Vite template
+# Portfolio
 
-## Features
-
-This template comes with the following features:
-
-- [PostCSS](https://postcss.org/) with [mantine-postcss-preset](https://mantine.dev/styles/postcss-preset)
-- [TypeScript](https://www.typescriptlang.org/)
-- [Storybook](https://storybook.js.org/)
-- [Vitest](https://vitest.dev/) setup with [React Testing Library](https://testing-library.com/docs/react-testing-library/intro)
-- ESLint setup with [eslint-config-mantine](https://github.com/mantinedev/eslint-config-mantine)
-
-## npm scripts
+> Built with Mantine and Vite
 
 ## Build and dev scripts
 
@@ -22,10 +12,10 @@ This template comes with the following features:
 
 - `typecheck` – checks TypeScript types
 - `lint` – runs ESLint
-- `prettier:check` – checks files with Prettier
+- `prettier` – checks files with Prettier
 - `vitest` – runs vitest tests
 - `vitest:watch` – starts vitest watch
-- `test` – runs `vitest`, `prettier:check`, `lint` and `typecheck` scripts
+- `test` – runs `vitest`, `prettier`, `lint` and `typecheck` scripts
 
 ### Other scripts
 
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -4,29 +4,25 @@ import classes from './Project.module.css';
 const data = [
   {
     title: 'Sistem Informasi Perangkat Pembelajaran',
-    image:
-      'https://picsum.photos/id/0/1280/720.webp',
+    image: 'https://picsum.photos/id/0/1280/720.webp',
     desc: 'An internal information system for school to streamline educational resource management.Built with Go, Vue.js framework, and PostgreSQL',
     link: 'https://github.com/latoulicious/SIPP', // Add your GitHub link here
   },
   {
     title: 'Healthie',
-    image:
-      'https://picsum.photos/id/122/1280/720.webp',
+    image: 'https://picsum.photos/id/122/1280/720.webp',
     desc: 'An internal information system for clinics to streamline clinic operations. Built with PHP, Yii2 framework, and PostgreSQL',
     link: 'https://github.com/latoulicious/Healthie', // Add your GitHub link here
   },
   {
     title: 'Lorem ipsum dolor sit amet',
-    image:
-      'https://picsum.photos/seed/imgur/1280/720.webp',
+    image: 'https://picsum.photos/seed/imgur/1280/720.webp',
     desc: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     link: '/', // Add your GitHub link here
   },
   {
     title: 'Lorem ipsum dolor sit amet',
-    image:
-      'https://picsum.photos/seed/unsplash/1280/720.webp',
+    image: 'https://picsum.photos/seed/unsplash/1280/720.webp',
     desc: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     link: '/', // Add your GitHub link here
   },
@@ -34,7 +30,15 @@ const data = [
 
 export function ProjectGrid() {
   const cards = data.map((article) => (
-    <Card key={article.title} p="md" radius="md" component="a" href={article.link} target="_blank" className={classes.card}>
+    <Card
+      key={article.title}
+      p="md"
+      radius="md"
+      component="a"
+      href={article.link}
+      target="_blank"
+      className={classes.card}
+    >
       <AspectRatio ratio={1920 / 1080}>
         <Image src={article.image} />
       </AspectRatio>
