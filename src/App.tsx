ff1e6aa8bf06f6f32322469f68950c646d0acd1e
arsyds <arsyds@proton.me>
2024-06-15 11:00:21 +0700

    feat: implement project pages content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from '@mantine/core';
 import { Router } from './router/Router';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+  fontFamily: 'Greycliff CF, sans-serif',
+  colors: {
+    'light-blue': [
+      '#e0fbff',
+      '#cbf2ff',
+      '#9ae2ff',
+      '#64d2ff',
+      '#3cc5fe',
+      '#23bcfe',
+      '#09b8ff',
+      '#00a1e4',
+      '#0090cd',
+      '#007cb5',
+    ],
+    'ocean-blue': [
+      '#7AD1DD',
+      '#5FCCDB',
+      '#44CADC',
+      '#2AC9DE',
+      '#1AC2D9',
+      '#11B7CD',
+      '#09ADC3',
+      '#0E99AC',
+      '#128797',
+      '#147885',
+    ],
+    'deep-blue': [
+      '#ebefff',
+      '#d5dafc',
+      '#a9b1f1',
+      '#7b87e9',
+      '#5362e1',
+      '#3a4bdd',
+      '#2d3fdc',
+      '#1f32c4',
+      '#182cb0',
+      '#0b259c',
+    ],
+    'bright-pink': [
+      '#F0BBDD',
+      '#ED9BCF',
+      '#EC7CC3',
+      '#ED5DB8',
+      '#F13EAF',
+      '#F71FA7',
+      '#FF00A1',
+      '#E00890',
+      '#C50E82',
+      '#AD1374',
+    ],
+  },
+});
 
 export default function App() {
   return (
-    <MantineProvider defaultColorScheme="auto">
+    <MantineProvider theme={theme}>
       <Router />
     </MantineProvider>
   );
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,44 +1,48 @@
 import { SimpleGrid, Card, Image, Text, Container, AspectRatio } from '@mantine/core';
 import classes from './Project.module.css';
 
-const mockdata = [
+const data = [
   {
-    title: 'Top 10 places to visit in Norway this summer',
+    title: 'Sistem Informasi Perangkat Pembelajaran',
     image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 18, 2022',
+      'https://picsum.photos/id/0/1280/720.webp',
+    desc: 'An internal information system for school to streamline educational resource management.Built with Go, Vue.js framework, and PostgreSQL',
+    link: 'https://github.com/latoulicious/SIPP', // Add your GitHub link here
   },
   {
-    title: 'Best forests to visit in North America',
+    title: 'Healthie',
     image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'August 27, 2022',
+      'https://picsum.photos/id/122/1280/720.webp',
+    desc: 'An internal information system for clinics to streamline clinic operations. Built with PHP, Yii2 framework, and PostgreSQL',
+    link: 'https://github.com/latoulicious/Healthie', // Add your GitHub link here
   },
   {
-    title: 'Hawaii beaches review: better than you think',
+    title: 'Lorem ipsum dolor sit amet',
     image:
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 9, 2022',
+      'https://picsum.photos/seed/imgur/1280/720.webp',
+    desc: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    link: '/', // Add your GitHub link here
   },
   {
-    title: 'Mountains at night: 12 best locations to enjoy the view',
+    title: 'Lorem ipsum dolor sit amet',
     image:
-      'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
-    date: 'September 12, 2022',
+      'https://picsum.photos/seed/unsplash/1280/720.webp',
+    desc: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    link: '/', // Add your GitHub link here
   },
 ];
 
 export function ProjectGrid() {
-  const cards = mockdata.map((article) => (
-    <Card key={article.title} p="md" radius="md" component="a" href="#" className={classes.card}>
+  const cards = data.map((article) => (
+    <Card key={article.title} p="md" radius="md" component="a" href={article.link} target="_blank" className={classes.card}>
       <AspectRatio ratio={1920 / 1080}>
         <Image src={article.image} />
       </AspectRatio>
-      <Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
-        {article.date}
-      </Text>
-      <Text className={classes.title} mt={5}>
+      <Text className={classes.title} size="xs" tt="uppercase" fw={700} mt="md">
         {article.title}
+      </Text>
+      <Text c="dimmed" mt={5}>
+        {article.desc}
       </Text>
     </Card>
   ));
